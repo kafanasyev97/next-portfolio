1d1b60a3a3a0e615f4577b2771229e290bf01b66
Kirill <kirillmas222@gmail.com>
2024-08-07 00:14:14 +0400

    [upd] Framer-motion animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: '1c1c22',
+        primary: '#1c1c22',
         accent: {
           DEFAULT: '#00ff99',
           hover: '#00e187',
